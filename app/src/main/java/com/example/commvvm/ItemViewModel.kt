cd9d866ce3fe1a837605ec50aa27c3ecd99d3028
blastka <svetuk505@mail.ru>
2024-03-29 16:51:15 +0300

    first commit

---
+++ app/src/main/java/com/example/commvvm/ItemViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.commvvm
+
+import android.content.ClipData
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ItemViewModel : ViewModel() {
+    private val mutableSelectedItem = MutableLiveData<String>()
+    val selectedItem: LiveData<String> get() = mutableSelectedItem
+
+    fun selectItem(item: String) {
+        mutableSelectedItem.value = item
+    }
+}
