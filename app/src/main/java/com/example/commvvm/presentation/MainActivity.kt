ebb341d0d7dfbe3bd4557e8d3a87e130ad3d1306
blastka <svetuk505@mail.ru>
2024-04-05 15:45:59 +0300

    equality

--- app/src/main/java/com/example/commvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/commvvm/presentation/MainActivity.kt
@@ -5,43 +5,72 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Observer
 
 
-class MainActivity : AppCompatActivity() {
-
-    private val viewModel: ItemViewModel by viewModels()
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Row {
-                Text(text = "kokee")
-                Text(text = "hello")
-                Text(text = "alufalifa")
-            }
+            Create()
         }
-        viewModel.selectedItem.observe(this, Observer { item ->
-            setContent {
-                NewTextTwo()
-            }
-        })
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun NewText(){
-    Text(text = "hello")
+@Preview
+fun TestPReview(){
+    Create()
 }
 
 @Composable
-fun NewTextTwo(){
-    Text(text = "hello all")
+fun Create() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(0.5f)
+                .background(Color.Blue),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "kokee1")
+            Text(text = "hello1")
+            Text(text = "alufalifa1")
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "kokee2")
+            Text(text = "hello2")
+            Text(text = "alufalifa2")
+        }
+    }
 }
